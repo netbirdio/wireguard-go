8c9a58056af76913c789d86486c676b0a26e1f26
Viktor Liu <viktor@netbird.io>
2025-07-09 11:50:34 +0200

    Add IPv6 support to receiver creator

--- conn/receiver_creator.go
+++ conn/receiver_creator.go
@@ -4,9 +4,14 @@ import (
 	"net"
 	"sync"
 
-	"golang.org/x/net/ipv4"
+	"golang.org/x/net/ipv6"
 )
 
+// PacketReader abstracts ipv4.PacketConn and ipv6.PacketConn which both implement ReadBatch
+type PacketReader interface {
+	ReadBatch([]ipv6.Message, int) (int, error)
+}
+
 type ReceiverCreator interface {
-	CreateIPv4ReceiverFn(pc *ipv4.PacketConn, conn *net.UDPConn, rxOffload bool, msgPool *sync.Pool) ReceiveFunc
+	CreateReceiverFn(pc PacketReader, conn *net.UDPConn, rxOffload bool, msgPool *sync.Pool) ReceiveFunc
 }
